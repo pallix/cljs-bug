2175b6f397ff17ebc7541f58cd0f3c38a945fa81
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2013-10-30 15:58:44 +0100

    update to latest lein-cljsbuild

--- project.clj
+++ project.clj
@@ -1,16 +1,13 @@
 (defproject cljs-bug "0.1.0-SNAPSHOT"
   :description "Project reproducing a ClojureScript or lein cljsbuild bug causing JS files to not be compiled"
-  :dependencies [[org.clojure/clojure "1.5.1"]]
-  :plugins [[lein-cljsbuild "0.3.0"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-1859"]]
+  :plugins [[lein-cljsbuild "0.3.4"]]
   :cljsbuild {:builds
-              [{:id "dev",
-                :source-paths ["src-cljs"
-                               ;; "clojurescript/src/clj"
-                               ;; "clojurescript/src/cljs"
-                               ],
-                :compiler {:pretty-print true,
-                           :libs ["src-js/libs"]
-                           :output-to "resources/compiled-app.js",
-                           :optimizations :whitespace},
-                :jar true}
-               ]})
+              {:dev {:source-paths ["src-cljs"],
+                     :compiler {:pretty-print true,
+                                :libs ["src-js/libs"]
+                                :output-to "resources/compiled-app.js",
+                                :optimizations :whitespace},
+                     :jar true}}
+              })
